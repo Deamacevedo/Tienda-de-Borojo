5a92c8d4a5302b18fd891d865cf6569d0fd382bc
Davisson-Adriel <droman819@unab.edu.co>
2025-08-24 13:44:13 -0500

    feat: :sparkles: Operadores de Arrays
    Se agrega el archivo correspondiente a la implementación de los operadores para arrays

---
+++ operadores_arrays.js
@@ -0,0 +1,14 @@
+// Ejercicios con operadores en consultas sobre arrays
+
+
+// 1. Buscar clientes que tengan "natural" en sus preferencias
+
+db.clientes.find({ "preferencias": "natural" });
+
+// 2. Encontrar productos que tengan al menos los tags "natural" y "orgánico" (usa $all)
+
+db.productos.find({ "tags": { $all: ["natural", "orgánico"] } });
+
+// 3. Listar productos que tienen más de un tag ($size)
+
+db.productos.find({ $expr: { $gt: [{ $size: "$tags" }, 1] } });
