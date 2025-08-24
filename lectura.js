2a4cdd61373d7f6475295c2abcb618c6411ef7ae
Jefersonlopezr <jefersonlopezr99@gmail.com>
2025-08-24 07:21:35 -0500

    feat: :art: Implementar operaciones de consulta para productos y clientes

---
+++ lectura.js
@@ -0,0 +1,9 @@
+// Ejercicios de lectura - CRUD Read
+
+// 1. Consultar todos los productos que tengan stock mayor a 20 unidades
+
+db.productos.find({"stock": {$gt: 20}});
+
+// 2. Encontrar los clientes que no han comprado aún ningún producto
+
+db.clientes.find({"compras": {$size: 0}});
